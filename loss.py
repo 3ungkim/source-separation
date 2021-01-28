d11327703b5095a01f01a52e6c7f46b2eb1f646b
3ungkim <eungbeom.k@gmail.com>
2021-01-29 01:31:03 +0900

    build train and loss(pit) without model

--- loss.py
+++ loss.py
@@ -2,13 +2,41 @@ import torch
 
 
 #signal distortion rate(sdr)
-#est, target = [batch, leng_signal]
-def SDRLoss(est, target):
-    dot = torch.sum(torch.mul(est, target), dim=1)
-    est_s = torch.sqrt(torch.sum(target**2, dim=1))
-    target_s = torch.sqrt(torch.sum(target**2, dim=1))
-    sdr = -torch.div(dot, torch.mul(est_s, target_s)) #sdr = [batch]
-    sdr_sum = torch.sum(sdr)
-    return sdr_sum
+#est, src = [batch, leng_signal]
+def SDRLoss(est, src):
+    dot = torch.sum(torch.mul(est, src), dim=1)
+    est_s = torch.sqrt(torch.sum(src**2, dim=1))
+    src_s = torch.sqrt(torch.sum(src**2, dim=1))
+    sdr = -torch.div(dot, torch.mul(est_s, src_s)) #sdr = [batch]
+    return sdr
 
-def pit(loss):
+
+def pit(loss, dic_est_src):
+    """
+    dic_est_src = {
+        "est": [list of est tensors=[batch, time]], "src": [list of sources=[batch,time]]
+    }
+    loss means a loss function to use
+    output is the min loss
+    """
+    est_set = dic_est_src["est"]
+    src_set = dic_est_src["src"]
+
+    #est_permute 필요
+    #est_permute = 
+
+    #fix src and permute est so that we can calculate all permutation of est and src
+    #est_list = [list of est tensor=[batch, time]]
+    for i, est_list in enumerate(est_permute):
+        batch = est_list[0].shape[0]
+        loss_temp = torch.zeros(batch)
+        for est, src in zip(est_list, src_set):
+            loss_temp = torch.add(loss_temp, loss(est, src))
+
+        if i==0:
+            loss_permute = loss_temp
+        else:
+            #loss_permute = [batch]
+            loss_permute = torch.minimum(loss_permute, loss_temp)
+
+    return torch.sum(loss_permute)
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from loss import SDRLoss
+from loss import pit
 from data.mkdata import VCTKDataset
 from torch.utils.data import DataLoader
 import argparse
@@ -31,14 +32,35 @@ scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
 for epoch in range(60):
     model.train()
     running_loss = 0.0
+    #src_list = []
+    #for i, data in enumerate(train_loader):
+    #   if i%2==0:
+    #       src_list.append(data)
+    #       continue
+    #   else:
+    #       src_list.append(data)
+    #mix = src_list[0] + src_list[1] 
     train1 = iter(train_loader1)
     train2 = iter(train_loader2)
-    for i in range(2000):
-        source1 = train1.next()
-        source2 = train2.next()
-        mix = source1 + source2
+    for i in range(2500):
+        src1 = train1.next()
+        src2 = train2.next()
+        src_list = [src1, src2]
+        mix = src1 + src2
 
         optimizer.zero_grad()
 
         est1, est2 = model(mix)
-        
+        est_list = [est1, est2]
+        dic_est_src = {"est": est_list, "src": src_list}
+
+        loss = pit(SDRLoss, dic_est_src)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i%500==499:
+            print(f'Train Epoch: {epoch+1}/60 {i+1}/2500\tLoss: {running_loss/500}')
+            running_loss = 0.0
+
+    scheduler.step()
