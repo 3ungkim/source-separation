913e7612f3886bf5aaa0ae73fec2c65ccd311678
eungkim <eungbeom.k@gmail.com>
2021-02-01 15:51:11 +0900

    build WaveUNet model without crop

--- model/waveunet.py
+++ model/waveunet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import copy
 
 
 class DownBlock(nn.Module):
@@ -18,23 +19,112 @@ class DownBlock(nn.Module):
         out2 = out1[:,:,0::2]
         return out1, out2
 
+
 class UpBlock(nn.Module):
-    def __init__(self, in_channel, out_channel, size_conv=5):
+    def __init__(self, in_channel, out_channel, up_channel, size_conv=5):
         self.in_channel = in_channel
         self.out_channel = out_channel
+        self.up_channel - up_channel
         self.size_conv = size_conv
+        self.upsample = nn.Conv1d(up_channel, up_channel, 2)
         self.conv = nn.Conv1d(in_channel, out_channel, size_conv)
     
     def forward(self, x, dsblock):
-        #upsample
-        #upsample된 크기의 tensor 만들고 tensor[:,:,0::2] = 기존 tensor[:,:,1:2] = interpolation result 해서 하나로 만들기
-        #concat
-        out = self.conv(x)
+        #Upsample1
+        #learned linear interpolation can be interpreted as conv, wft + (1-w)ft+1
+        regul = torch.sum(self.upsample.weight, dim=-1, keepkim=True)
+        param = torch.div(self.upsample.weight, regul)
+        self.upsample.weight = nn.parameter.Parameter(param)
+        interpol = self.upsample(x)
+        
+        #Upsample2
+        #concatenate origin tensor and upsampled tensor(interpolated tensor)
+        shape = list(x.shape)
+        shape[-1] = shape[-1]*2
+        out = torch.zeros(shape)
+        out[:,:,0::2] = x
+        out[:,:,1::2] = interpol
+
+        #Concat
+        out = torch.cat((x, dsblock), dim=1)
+
+        #Conv1d
+        out = self.conv(out)
+
+        return out
+
 
 class WaveUNet(nn.Module):
-    def __init__(self, layer_num, size_conv):
+    def __init__(self, layer_num=12, size_dsconv=15, size_usconv=5, channel_size=24, source_num=2):
         super(WaveUNet, self).__init__()
         self.layer_num = layer_num
-        self.size_conv = size_conv
+        self.size_dsconv = size_dsconv
+        self.size_usconv = size_usconv
+        self.channel_size = channel_size
 
-    def forward(self, mix):
+        self.dsblock_list = self._repeat_dsblock(
+            layer_num=layer_num,
+            size_conv=size_dsconv, 
+            channel_size=channel_size
+        )
+        self.conv1 = nn.Conv1d(
+            in_channels=channel_size*layer_num,
+            out_channels=channel_size*(layer_num+1),
+            kernel_size=size_dsconv
+        ) 
+        self.usblock_list = self._repeat_usblock(
+            layer_num=layer_num,
+            size_conv=size_usconv,
+            channel_size=channel_size
+        )
+        self.conv2 = nn.Conv1d(
+            in_channels=channel_size+1,
+            out_channels=source_num-1,
+            kernel_size=1
+        )
+
+
+    def forward(self, x):
+        init_val = copy.deepcopy(x)
+        #downsample block
+        dscrop_list = []
+        for dsblock in self.dsblock_list:
+            dscrop, x = dsblock(x)
+            dscrop_list.append(dscrop)
+
+        #middle block
+        x = self.conv1(x)
+
+        #upsample block
+        ################################################block concat size 맞추기
+        dscrop_list.reverse()
+        for usblock, dscrop in zip(self.usblock_list, dscrop_list):
+            x = usblock(x, dscrop)
+        
+        #final block
+        ################################################block concat size 맞추기
+        out = torch.cat((x, init_val), dim=1)
+        out = self.conv2(out)
+
+    def _repeat_dsblock(self, layer_num=12, size_conv=15, channel_size=24):
+        dsblock_list = nn.ModuleList([DownBlock(1, channel_size, size_conv)])
+        for i in range(1, layer_num):
+            dsblock_list.append(DownBlock(
+                in_channel=channel_size*i,
+                out_channel=channel_size*(i+1),
+                size_conv=size_conv
+            )) 
+        return dsblock_list
+
+    def _repeat_usblock(self, layer_num=12, size_conv=5, channel_size=24):
+        #UpBlock(in_channel, out_channel, up_channel, size_conv)
+        usblock_list = nn.ModuleList()
+        for i in range(layer_num, 0):
+            usblock_list.append(UpBlock(
+                in_channel=channel_size*(2*layer_num+1),
+                out_channel=channel_size*layer_num,
+                up_channel=channel_size*(layer_num+1),
+                size_conv=size_conv
+            ))
+        return usblock_list
+
